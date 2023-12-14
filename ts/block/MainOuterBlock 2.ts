0e9abeb445c32f0f1035df050297ec72a93e8c48
K6321030 <146102879+K6321030@users.noreply.github.com>
2023-12-14 12:49:05 +0900

    Create MainOuterBlock 2.ts

---
+++ ts/block/MainOuterBlock 2.ts
@@ -0,0 +1,25 @@
+import {SimpleOuterBlock} from "./SimpleOuterBlock";
+import {IBlockPosition} from "../interface/IBlockPosition";
+import {IExpression} from "../expression/interface/INode";
+import {FFunction, FFunctionBody} from "../expression/FNode";
+
+export class MainOuterBlock extends SimpleOuterBlock {
+  constructor(identifier: string, childrenPositions: IBlockPosition[]) {
+    super(identifier, childrenPositions);
+
+    const mainText = document.createElement('span')
+    mainText.innerText = "Main Function"
+    mainText.style.fontSize = '12px'
+    mainText.style.fontWeight = 'bold'
+    mainText.style.position = 'absolute'
+    mainText.style.top = '5px'
+    mainText.style.left = '5px'
+    this.element.appendChild(mainText)
+  }
+
+  public override getExpression(): IExpression {
+    return new FFunction("main", null,
+      new FFunctionBody(this.getChildrenExpression())
+    )
+  }
+}
